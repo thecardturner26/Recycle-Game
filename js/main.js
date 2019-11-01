d278e73359daf6dd071a0f9ef3bcc52ac6be75a6
Marco Arteaga <marcoarteaga26@gmail.com>
2019-11-01 11:56:15 -0500

    array data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 let reItems = ['bottle water', 'grocery bag', 'straw']
-let compItems = []
-let landItems = []
+let compItems = ['banana', 'egg shells', 'leafs']
+let landItems = ['clothes', 'batteries', 'furniture']

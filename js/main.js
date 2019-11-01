4c69d43fa767b7f8c7467425f4f6d9f81cda67c3
Marco Arteaga <marcoarteaga26@gmail.com>
2019-11-01 17:20:01 -0500

    array test

--- js/main.js
+++ js/main.js
@@ -3,12 +3,45 @@ var compost = ['banana', 'egg shells', 'leafs']
 var landfill = ['clothes', 'batteries', 'furniture']
 
 
-function displayWord(){
-    var h = "<hr/>";   
+//function displayWord(){
     
-   for (var y=0; y<array.length; y++)
-   {
-     e += "Element " + y + " = " + array[y] + "<br/>";
-   }
-   document.getElementById("Result").innerHTML = e;
-}
+// for (i = 0; i < recycle.length; i++){
+//     document.write(
+//         "  " + i + "  " +
+//         recycle[i] + "<br/>"
+//     )
+// }
+const messageEl = document.querySelector('h2');
+
+
+function addElement () { 
+    // create a new div element 
+    var newDiv = document.createElement("div"); 
+    // and give it some content 
+    var newContent = recycle.textContent = recycle.length; 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);  
+  
+    // add the newly created element and its content into the DOM 
+    var currentDiv = document.getElementById("div1"); 
+    document.body.insertBefore(newDiv, currentDiv); 
+  }
+
+
+
+
+
+  
+//for (i = 0; i < Colors.length; i++)
+//   <script language="JavaScript">
+//     // Create the array and fill it with data.
+//     var Colors = ["Blue", "Green", "Purple"];
+//     // Define a loop to access each array element
+//     // and display it onscreen.
+//     for (i = 0; i < Colors.length; i++)
+//     {
+//       document.write(
+//         "Colors " + i + " = " +
+//         Colors[i] + "<br />");
+//     }
+//   </script>

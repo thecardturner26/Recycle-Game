3656fb6ce03c42ec75939b73f92d13616e361ccf
Marco Arteaga <marcoarteaga26@gmail.com>
2019-11-01 13:03:31 -0500

    display word funtion test

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ var compost = ['banana', 'egg shells', 'leafs']
 var landfill = ['clothes', 'batteries', 'furniture']
 
 
-function qTerm(){
-
+function displayWord(){
+    var h = "<hr/>";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     e += "Element " + y + " = " + array[y] + "<br/>";
+   }
+   document.getElementById("Result").innerHTML = e;
 }

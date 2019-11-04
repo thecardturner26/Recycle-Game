39a07f00908e667126a6ca918d3a120fc7c39caf
Marco Arteaga <marcoarteaga26@gmail.com>
2019-11-04 15:10:54 -0600

    testing logic code and css

--- js/main.js
+++ js/main.js
@@ -1,85 +1,91 @@
-// var recycle = ['bottle water', 'grocery bag', 'straw']
-// var compost = ['banana', 'egg shells', 'leafs']
-// var landfill = ['clothes', 'batteries', 'furniture']
 
 
-
-// $(":input").change(function(){
-//     $(this).parents(".questionContainer").addClass("hideMe");
-//     $(this).parents(".questionContainer ").next(".questionContainer ").removeClass("hideMe");
-// });
-
-
-
-
-// var ul=document.getElementById('ul');
-// var btn=document.getElementById('button');
-// var scoreCard=document.getElementById('scoreCard');
-// var quizBox=document.getElementById('questionBox');
+var ul=document.getElementById('ul');
+var btn=document.getElementById('button');
+var scoreCard=document.getElementById('scoreCard');
+var quizBox=document.getElementById('questionBox');
 var op1=document.getElementById('op1');
 var op2=document.getElementById('op2');
 var op3=document.getElementById('op3');
-var op4=document.getElementById('op4');
+
 
 
 
 var app={
     questions:[
-              {q:'Bottle water?', options:['Recycle','Compost','Landfill',],answer:1},
+                {q:'Bottle water?', options:['Recycle','Compost','Landfill',],answer:1},
 
-              {q:'Grocery bag ?',options:['Recycle','Compost','Landfill',],answer:1},
+                {q:'Grocery bag ?',options:['Recycle','Compost','Landfill',],answer:1},
 
-              {q:'Staw?',options:['Recycle','Compost','Landfill'],answer:1}
-              ],
+                {q:'Staw?',options:['Recycle','Compost','Landfill'],answer:1}
+             ],
 
+              index:0,
+              load:function(){
+                     if(this.index<=this.questions.length-1){
+                      quizBox.innerHTML=this.index+1+". "+this.questions[this.index].q;      
+                      op1.innerHTML=this.questions[this.index].options[0];
+                      op2.innerHTML=this.questions[this.index].options[1];
+                      op3.innerHTML=this.questions[this.index].options[2];
+                         this.scoreCard();
+                      }
+                      else{
 
-
-
-
-
-
-
-
-
-//function displayWord(){
-
-// for (i = 0; i < recycle.length; i++){
-//     document.write(
-//         "  " + i + "  " +
-//         recycle[i] + "<br/>"
-//     )
-// }
-// const messageEl = document.querySelector('h2');
-
-
-// function addElement () { 
-//     // create a new div element 
-//     var newDiv = document.createElement("div"); 
-//     // and give it some content 
-//     var newContent = recycle.textContent = recycle.length; 
-//     // add the text node to the newly created div
-//     newDiv.appendChild(newContent);  
-  
-//     // add the newly created element and its content into the DOM 
-//     var currentDiv = document.getElementById("div1"); 
-//     document.body.insertBefore(newDiv, currentDiv); 
-//   }
-
-
-
-
-
-
-//for (i = 0; i < Colors.length; i++)
-//   <script language="JavaScript">
-//     // Create the array and fill it with data.
-//     var Colors = ["Blue", "Green", "Purple"];
-//     // Define a loop to access each array element
-//     // and display it onscreen.
-//     for (i = 0; i < Colors.length; i++)
-//     {
-//       document.write(
-//         "Colors " + i + " = " +
-//         Colors[i] + "<br />");
-//     }
-//   </script>
+                      quizBox.innerHTML="Game Over......!!!"      
+                      op1.style.display="none";
+                      op2.style.display="none";
+                      op3.style.display="none";
+                      btn.style.display="none";
+                      }
+                    },
+                    next:function(){
+                       this.index++;
+                       this.load();
+                    },
+                   check:function(ele){
+                      
+                            var id=ele.id.split('');
+                            
+                            if(id[id.length-1]==this.questions[this.index].answer){
+                                this.score++;
+                                ele.className="correct";
+                                ele.innerHTML="Correct";
+                                this.scoreCard();
+                            }
+                            else{
+                                ele.className="wrong";
+                                ele.innerHTML="Wrong";
+                            }
+                   },
+                   notClickAble:function(){
+                          for(let i=0;i<ul.children.length;i++){
+                                  ul.children[i].style.pointerEvents="none";
+                          }
+                   },
+   
+                   clickAble:function(){
+                          for(let i=0;i<ul.children.length;i++){
+                                  ul.children[i].style.pointerEvents="auto";
+                                  ul.children[i].className=''
+   
+                          }
+                   },
+                   score:0,
+                   scoreCard:function(){
+                       scoreCard.innerHTML=this.questions.length+"/"+this.score;
+                   }
+    
+              }
+   
+   
+              window.onload=app.load();
+   
+              function button(ele){
+                       app.check(ele);
+                       app.notClickAble();
+              }
+   
+            function  next(){
+                 app.next();
+                 app.clickAble();
+            }   

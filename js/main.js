d9e6a164d907d352f1ef97ea6938ccfe3c432dab
Marco Arteaga <marcoarteaga26@gmail.com>
2019-11-04 12:43:13 -0600

    new array for questions and answrs

--- js/main.js
+++ js/main.js
@@ -4,10 +4,35 @@
 
 
 
-$(":input").change(function(){
-    $(this).parents(".questionContainer").addClass("hideMe");
-    $(this).parents(".questionContainer ").next(".questionContainer ").removeClass("hideMe");
-});
+// $(":input").change(function(){
+//     $(this).parents(".questionContainer").addClass("hideMe");
+//     $(this).parents(".questionContainer ").next(".questionContainer ").removeClass("hideMe");
+// });
+
+
+
+
+// var ul=document.getElementById('ul');
+// var btn=document.getElementById('button');
+// var scoreCard=document.getElementById('scoreCard');
+// var quizBox=document.getElementById('questionBox');
+var op1=document.getElementById('op1');
+var op2=document.getElementById('op2');
+var op3=document.getElementById('op3');
+var op4=document.getElementById('op4');
+
+
+
+var app={
+    questions:[
+              {q:'Bottle water?', options:['Recycle','Compost','Landfill',],answer:1},
+
+              {q:'Grocery bag ?',options:['Recycle','Compost','Landfill',],answer:1},
+
+              {q:'Staw?',options:['Recycle','Compost','Landfill'],answer:1}
+              ],
+
+
 
 
 
